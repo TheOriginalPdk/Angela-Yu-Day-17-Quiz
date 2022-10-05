4154ad488ff798f50bc7f57abee5313dee1f3fb2
unknown <weerasinghe.panduka@gmail.com>
2022-10-05 15:14:47 +0530

    AY Python D17 quiz part 4

---
+++ main.py
@@ -0,0 +1,15 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import Quizbrain
+
+question_bank = []
+for i in question_data:
+    question_text = i["text"]
+    question_answer = i["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+print(question_bank[4].text)
+quiz=Quizbrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
